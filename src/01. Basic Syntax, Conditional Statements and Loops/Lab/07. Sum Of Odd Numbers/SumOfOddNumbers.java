1092088638f8f980e04323823312e949d0e13b03
stiliyan <stiliyan_2001@abv.bg>
2023-10-07 18:22:08 +0300

    Sum of Odd Numbers Task

---
+++ src/01. Basic Syntax, Conditional Statements and Loops/Lab/07. Sum Of Odd Numbers/SumOfOddNumbers.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SumOfOddNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int endIndex = Integer.parseInt(scanner.nextLine());
+        int oddSum = 0;
+
+        for (int i = 1; i <= endIndex; i++) {
+            int oddNumber = i * 2 - 1;
+            System.out.println(oddNumber);
+            oddSum += oddNumber;
+        }
+
+        System.out.println(oddSum);
+    }
+}
