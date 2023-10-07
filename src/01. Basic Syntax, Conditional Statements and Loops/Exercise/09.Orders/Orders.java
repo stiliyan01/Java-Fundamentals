fd7e85548c13d10c638c9c2ac55f3471a2f957e5
stiliyan <stiliyan_2001@abv.bg>
2023-10-07 22:23:03 +0300

    Orders Task

---
+++ src/01. Basic Syntax, Conditional Statements and Loops/Exercise/09.Orders/Orders.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+
+public class Orders {
+    public static void main(String[] args) {
+
+        // ((daysInMonth * capsulesCount)*pricePerCapsule
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        double result = 0;
+        for (int i = 0; i < n; i++) {
+            double pricePerCapsule = Double.parseDouble(scanner.nextLine());
+
+            int daysInMonth = Integer.parseInt(scanner.nextLine());
+            int capsulesCount = Integer.parseInt(scanner.nextLine());
+            System.out.println("tuk");
+            result = ((daysInMonth * capsulesCount) * pricePerCapsule);
+
+        }
+
+        System.out.println("Total: "+ result);
+
+    }
+}
