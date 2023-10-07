4c597cc28e55cbd3bb5fcb4c0066757c3ecde122
stiliyan <stiliyan_2001@abv.bg>
2023-10-07 18:31:06 +0300

    Even Number Task

---
+++ src/01. Basic Syntax, Conditional Statements and Loops/Lab/10. Even Number/EvenNumber.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class EvenNumber {
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        int inputNumber = Integer.parseInt(scanner.nextLine());
+
+        while (inputNumber % 2 != 0) {
+            System.out.println("Please write an even number");
+            inputNumber = Integer.parseInt(scanner.nextLine());
+        }
+
+        System.out.println("The number is: " + Math.abs(inputNumber));
+    }
+}
