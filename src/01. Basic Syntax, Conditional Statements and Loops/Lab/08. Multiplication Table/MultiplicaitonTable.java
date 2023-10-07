35defbe059b18dff6c5c3c94eb5fdce1a6606936
stiliyan <stiliyan_2001@abv.bg>
2023-10-07 18:26:51 +0300

    Multiplication Table Task

---
+++ src/01. Basic Syntax, Conditional Statements and Loops/Lab/08. Multiplication Table/MultiplicaitonTable.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class MultiplicaitonTable {
+    private static final int START_NUMBER = 1;
+    private static final int END_NUMBER = 10;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = Integer.parseInt(scanner.nextLine());
+
+        for (int i = START_NUMBER; i <= END_NUMBER; i++) {
+            System.out.printf("%d X %d = %d%n", number, i, number * i);
+        }
+    }
+}
