2c94b0f7b4212ce14cf5b94e5ed8921fa3f7b71a
stiliyan <stiliyan_2001@abv.bg>
2023-10-10 11:55:51 +0300

    Reversed Chars Task

---
+++ src/02. Data Types and Variables/Lab/07. Reversed Chars/ReversedChars.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ReversedChars {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        char firstChar = readCharFromConsole(scanner);
+        char secondChar = readCharFromConsole(scanner);
+        char thirdChar = readCharFromConsole(scanner);
+
+        printReversedOutput(thirdChar, secondChar, firstChar);
+    }
+
+    private static char readCharFromConsole(Scanner scanner) {
+        return scanner.nextLine().charAt(0);
+    }
+
+    private static void printReversedOutput(char firstChar, char secondChar, char thirdChar) {
+        System.out.printf("%c %c %c", firstChar, secondChar, thirdChar);
+
+    }
+}
