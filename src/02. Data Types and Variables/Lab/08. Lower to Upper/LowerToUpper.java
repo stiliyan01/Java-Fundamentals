e0a4a97d9eafa2ca3bb656d22c02025ce9477a1b
stiliyan <stiliyan_2001@abv.bg>
2023-10-10 11:56:58 +0300

    Lower To Upper Task

---
+++ src/02. Data Types and Variables/Lab/08. Lower to Upper/LowerToUpper.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class LowerToUpper {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        printResult(input);
+
+    }
+
+    private static void printResult(String input) {
+        if (input.equals(input.toLowerCase())){
+            System.out.println("lower-case");
+        } else if (input.equals(input.toUpperCase())){
+            System.out.println("upper-case");
+        }
+
+    }
+}
