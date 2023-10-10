3a44c9d9755aed8c23a4d2aff943e27052fa62fd
stiliyan <stiliyan_2001@abv.bg>
2023-10-10 11:49:20 +0300

    Pounds to Dollars Task

---
+++ src/02. Data Types and Variables/Lab/02.Pounds to Dollars/PoundsToDollars.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class PoundsToDollars {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int currencyInput = Integer.parseInt(scanner.nextLine());
+        double poundsInUSD = calculateExchangeRate(currencyInput);
+        printResult(poundsInUSD);
+    }
+
+    private static void printResult(double poundsInUSD) {
+        System.out.printf("%.3f", poundsInUSD);
+    }
+
+    private static double calculateExchangeRate(int currencyInput) {
+        double rate = 1.31;
+        return currencyInput * rate;
+    }
+}
+
