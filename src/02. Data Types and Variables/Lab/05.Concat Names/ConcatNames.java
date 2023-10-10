95582a8f196c82d3bed98e4d2fc2396ec07e8e93
stiliyan <stiliyan_2001@abv.bg>
2023-10-10 11:54:37 +0300

    Concat Names Task

---
+++ src/02. Data Types and Variables/Lab/05.Concat Names/ConcatNames.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ConcatNames {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String firstNameInput = readStringFromConsole(scanner);
+        String secondNameInput = readStringFromConsole(scanner);
+        String delimiterInput = readStringFromConsole(scanner);
+
+        printOutput(firstNameInput, secondNameInput, delimiterInput);
+    }
+
+    private static void printOutput(String firstNameInput, String secondNameInput, String delimiterInput) {
+        System.out.printf("%s%s%s", firstNameInput, delimiterInput, secondNameInput);
+    }
+
+    private static String readStringFromConsole(Scanner scanner) {
+        return scanner.nextLine();
+    }
+}
+
