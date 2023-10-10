ddf45e2d4e08a6283313d767fd9b9f7aaf7d6bb8
stiliyan <stiliyan_2001@abv.bg>
2023-10-10 11:53:07 +0300

    Town Info Task

---
+++ src/02. Data Types and Variables/Lab/04. Town Info/TownInfo.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class TownInfo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String city = scanner.nextLine();
+        int population = readIntegerInput(scanner);
+        int area = readIntegerInput(scanner);
+
+        printResult(city, population, area);
+    }
+
+    private static int readIntegerInput(Scanner scanner) {
+        return Integer.parseInt(scanner.nextLine());
+    }
+
+    private static void printResult(String city, int population, int area) {
+        System.out.printf("Town %s has population of %d and area %d square km.", city, population, area);
+    }
+
+}
