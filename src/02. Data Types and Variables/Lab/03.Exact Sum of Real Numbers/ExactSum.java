bba98f59bc5413950aab0322731189b1668baffe
stiliyan <stiliyan_2001@abv.bg>
2023-10-10 11:50:59 +0300

    Exact Sum Task

---
+++ src/02. Data Types and Variables/Lab/03.Exact Sum of Real Numbers/ExactSum.java
@@ -0,0 +1,25 @@
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class ExactSum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        BigDecimal sum = sumNumbers(n, scanner);
+
+        System.out.println(sum);
+    }
+
+    private static BigDecimal sumNumbers(int n, Scanner scanner) {
+        BigDecimal sum = new BigDecimal(0);
+
+        for (int i = 0; i < n; i++) {
+            BigDecimal numberInput = new BigDecimal(scanner.nextLine());
+            sum = sum.add(numberInput);
+        }
+
+        return sum;
+    }
+
+}
