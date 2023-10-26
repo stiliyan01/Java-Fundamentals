27611aad7836357760c6e0795c14a1dc09b1a918
stiliyan <stiliyan_2001@abv.bg>
2023-10-26 13:18:58 +0300

    Day of Week Task

---
+++ src/03.Arrays/Lab/01. Day of Week/DayOfWeek.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class DayOfWeek {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] daysOfWeek = {"Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday",
+        };
+
+        int n = Integer.parseInt(scanner.nextLine());
+        boolean validInput = checkIfInputIsValid(n);
+        printResult(validInput, n, daysOfWeek);
+    }
+
+    private static boolean checkIfInputIsValid(int n) {
+        return n >= 1 && n <= 7;
+    }
+
+    private static void printResult(boolean checkInput, int n, String[] daysOfWeek) {
+        if (!checkInput) {
+            System.out.println("Invalid day!");
+        } else {
+            System.out.println(daysOfWeek[n - 1]);
+        }
+    }
+}
