60fc32d70eb2be3c14afbaaf950038baa5e56dd1
stiliyan <stiliyan_2001@abv.bg>
2023-10-26 19:51:07 +0300

    Difference Even and Odd Sum Task

--- src/03.Arrays/Lab/03. Sum Even and Odd Numbers/SumEvenAndOdds.java
+++ src/03.Arrays/Lab/03. Sum Even and Odd Numbers/SumEvenAndOdds.java
@@ -1,27 +1,24 @@
 import java.util.Scanner;
 
-public class NumbersInReverseOrder {
+public class SumEvenAndOdds {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
         String value = scanner.nextLine();
         String[] numbersAsStrings = value.split("[,\\s]+");
         int [] numbers = new int[numbersAsStrings.length];
 
+        int sumOdds=0;
+        int sumEven=0;
+
+
         for (int i = 0; i < numbers.length; i++) {
             numbers[i]=Integer.parseInt(numbersAsStrings[i]);
+            if(numbers[i]%2==0){
+                sumEven+=numbers[i];
+            }else{
+                sumOdds+=numbers[i];
+            }
         }
-        for (int i = 0; i < numbers.length / 2; i++) {
-            int otherIndex = numbers.length - 1 - i;
-            int temp = numbers[i];
-            numbers[i] = numbers[otherIndex];
-            numbers[otherIndex] = temp;
-        }
-
-        for (int number : numbers) {
-            System.out.println(number);
-        }
-
-
+        System.out.println("The difference is: "+ (sumEven-sumOdds));
     }
 }
