81810345aa2bdaca31f86489b144dfb068372fb2
stiliyan <stiliyan_2001@abv.bg>
2023-10-29 11:50:04 +0200

    EvenOddSubstarcion Task

---
+++ src/03.Arrays/Lab/05. Even and Odd Substraction/EvenOddSubstarcion.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class EvenOddSubstarcion {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] inputNumbers = readIntArray(scanner);
+
+        int evenSum = Arrays.stream(inputNumbers).filter(e -> e % 2 == 0).sum();
+        int oddSum = Arrays.stream(inputNumbers).filter(e -> e % 2 != 0).sum();
+
+        System.out.println(evenSum - oddSum);
+    }
+
+    private static int[] readIntArray(Scanner scanner) {
+        return Arrays.stream(scanner.nextLine()
+                        .split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+    }
+}
