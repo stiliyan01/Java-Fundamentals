f6e97103cc8a8fde83a3efef130f90790a4d4114
stiliyan <stiliyan_2001@abv.bg>
2023-10-29 14:00:10 +0200

    Common Elements Task

---
+++ src/03.Arrays/Exercise/02. Common Elements/CommonElements.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class CommonElements {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+        String[] firstInput = scanner.nextLine().split("\\s+");
+        String[] secondInput = scanner.nextLine().split("\\s+");
+
+        for (int i = 0; i < firstInput.length; i++) {
+            for (int j = 0; j < secondInput.length; j++) {
+                if(secondInput[j].equals(firstInput[i])){
+                    System.out.print(firstInput[i] + " " );
+                }
+            }
+
+        }
+    }
+}
