a742fa17900a509476c6a93ac65c11469bdb7d6f
stiliyan <stiliyan_2001@abv.bg>
2023-11-13 22:13:27 +0200

    List Of Products Task

---
+++ src/05. Lists/Lab/06.List of Products/ListOfProducts.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class ListOfProducts {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        List<String> productsList = new ArrayList<>();
+
+        addProductsToList(scanner, n, productsList);
+        Collections.sort(productsList);
+        printResult(productsList);
+    }
+
+    private static void printResult(List<String> list) {
+        for (int i = 0; i < list.size(); i++) {
+            System.out.printf("%d.%s\n",i+1, list.get(i));
+        }
+    }
+
+    private static void addProductsToList(Scanner scanner, int n, List<String> list) {
+        for (int i = 0; i < n; i++) {
+            String product = scanner.nextLine();
+            list.add(product);
+        }
+    }
+}
