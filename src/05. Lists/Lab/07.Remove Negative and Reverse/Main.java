08d911e236ffcfcd565b22cc61cd53e0cada9174
stiliyan <stiliyan_2001@abv.bg>
2024-12-28 22:44:01 +0200

    Remove Negatives and Reverse

---
+++ src/05. Lists/Lab/07.Remove Negative and Reverse/Main.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numbersList = readList(scanner);
+
+        numbersList = filterListOfIntegers(numbersList);
+
+        Collections.reverse(numbersList);
+
+        printResult(numbersList);
+    }
+
+    private static List<Integer> readList(Scanner scanner) {
+        return Arrays.stream(scanner.nextLine()
+                        .split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
+    private static List<Integer> filterListOfIntegers(List<Integer> list) {
+        return list.stream().filter(e -> e > 0).collect(Collectors.toList());
+    }
+
+    private static void printResult(List<Integer> list) {
+        System.out.println(list.toString().replaceAll("[\\[\\],]", ""));
+    }
+}
